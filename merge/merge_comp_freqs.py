0e5b13cde774f93a487b520a4a5dd10012b03667
Adam Petersen <adam@adampetersen.se>
2014-06-13 14:10:10 +0200

    Sanity checks of the input data

--- merge/merge_comp_freqs.py
+++ merge/merge_comp_freqs.py
@@ -4,6 +4,10 @@
 import csv
 import sys
 
+class MergeError(Exception):
+	def __init__(self, message):
+		Exception.__init__(self, message)
+
 class Merged(object):
 	def __init__(self):
 		self._all_modules_with_complexity = {}
@@ -25,10 +29,23 @@ class Merged(object):
 def skip_heading(f):
 	next(f)
 	
-def parse_csv(merged, filename, parse_action):
+def read_heading_from(r):
+	p = r.next()
+	while p == []:
+		p = r.next()
+	return p
+
+def validate_content_by(heading, expected):
+	comparison = expected.split(',')
+	stripped = heading[0:len(comparison)] # allow extra fields
+	if stripped != comparison:
+		raise MergeError('Erroneous content. Expected = ' + expected + ', got = ' + ','.join(heading))
+
+def parse_csv(merged, filename, parse_action, expected_format):
 	with open(filename, 'rb') as csvfile:
 		r = csv.reader(csvfile, delimiter=',')
-		skip_heading(r)
+		heading = read_heading_from(r)
+		validate_content_by(heading, expected_format)
 		for row in r:
 			parse_action(merged, row)
 
@@ -50,12 +67,13 @@ def parse_freqs(merged, row):
 
 def merge(revs_file, comp_file):
 	merged = Merged()
-	parse_csv(merged, comp_file, parse_complexity)
-	parse_csv(merged, revs_file, parse_freqs)
+	parse_csv(merged, comp_file, parse_complexity, expected_format='language,filename,blank,comment,code')
+	parse_csv(merged, revs_file, parse_freqs, expected_format='entity,n-revs')
 	write_csv(merged.sorted_result())
 
 if __name__ == '__main__':
-	# TODO: check!
+	if len(sys.argv) != 3:
+		raise MergeError('Wrong arguments. Require one CSV file with frequencies and one with the complexity')
 	revs_file = sys.argv[1]
 	comp_file = sys.argv[2]
 	merge(revs_file, comp_file)
