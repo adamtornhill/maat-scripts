ec4e1cd9219ed598570bfbfdf2afdb57230cf469
Adam Petersen <adam@adampetersen.se>
2014-06-13 13:46:11 +0200

    Basic tests

--- merge/merge_comp_freqs.py
+++ merge/merge_comp_freqs.py
@@ -4,50 +4,58 @@
 import csv
 import sys
 
-p1 = {}
-p2 = {}
+class Merged(object):
+	def __init__(self):
+		self._all_modules_with_complexity = {}
+		self._merged = {}
 
-def cache_complexity(name, complexity):
-	default_size_metric = 0
-	p1[name] = complexity,default_size_metric
+	def sorted_result(self):
+		# Sort on descending order:
+		ordered = sorted(self._merged.items(), key=lambda item: item[1][0], reverse=True)
+		return ordered
 
-def extend_with(name, freqs):
-	if name in p1:
-		complexity, _ = p1[name]
-		p2[name] = freqs, complexity
+	def extend_with(self, name, freqs):
+		if name in self._all_modules_with_complexity:
+			complexity = self._all_modules_with_complexity[name]
+			self._merged[name] = freqs, complexity
+
+	def record_detected(self, name, complexity):
+		self._all_modules_with_complexity[name] = complexity
 	
 def skip_heading(f):
 	next(f)
 	
-def parse_csv(filename, parse_action):
+def parse_csv(merged, filename, parse_action):
 	with open(filename, 'rb') as csvfile:
 		r = csv.reader(csvfile, delimiter=',')
 		skip_heading(r)
 		for row in r:
-			parse_action(row)
+			parse_action(merged, row)
 
 def write_csv(stats):
 	print 'module,revisions,code'
-	# Sort on descending order:
-	ordered = sorted(stats.items(), key=lambda item: item[1][0], reverse=True)
-	for s in ordered:
+	for s in stats:
 		name, (f,c) = s
 		print name + ',' + f + ',' + c
 	
-def parse_complexity(row):
+def parse_complexity(merged, row):
 	name = row[1][2:]
 	complexity = row[4]
-	cache_complexity(name, complexity)
+	merged.record_detected(name, complexity)
 
-def parse_freqs(row):
+def parse_freqs(merged, row):
 	name = row[0]
 	freqs = row[1]
-	extend_with(name, freqs)
+	merged.extend_with(name, freqs)
 
-# TODO: check!
-revs_file = sys.argv[1]
-comp_file = sys.argv[2]
+def merge(revs_file, comp_file):
+	merged = Merged()
+	parse_csv(merged, comp_file, parse_complexity)
+	parse_csv(merged, revs_file, parse_freqs)
+	write_csv(merged.sorted_result())
 
-parse_csv(comp_file, parse_complexity)
-parse_csv(revs_file, parse_freqs)
-write_csv(p2)
+if __name__ == '__main__':
+	# TODO: check!
+	revs_file = sys.argv[1]
+	comp_file = sys.argv[2]
+	merge(revs_file, comp_file)
