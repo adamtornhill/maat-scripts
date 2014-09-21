9c8a5a6bb4bdb7e215678d228b39db964fe4cd60
Adam Petersen <adam@adampetersen.se>
2014-09-21 17:53:17 +0200

    Removed dead code

--- transform/csv_main_dev_as_knowledge_json.py
+++ transform/csv_main_dev_as_knowledge_json.py
@@ -17,50 +17,6 @@ import csv
 import json
 import sys
 
-class MergeError(Exception):
-	def __init__(self, message):
-		Exception.__init__(self, message)
-
-class Merged(object):
-	def __init__(self):
-		self._all_modules_with_complexity = {}
-		self._merged = {}
-
-	def sorted_result(self):
-		# Sort on descending order:
-		ordered = sorted(self._merged.items(), key=lambda item: item[1][0], reverse=True)
-		return ordered
-
-	def extend_with(self, name, freqs):
-		if name in self._all_modules_with_complexity:
-			complexity = self._all_modules_with_complexity[name]
-			self._merged[name] = freqs, complexity
-
-	def record_detected(self, name, complexity):
-		self._all_modules_with_complexity[name] = complexity
-
-def write_csv(stats):
-	print 'module,revisions,code'
-	for s in stats:
-		name, (f,c) = s
-		print name + ',' + f + ',' + c
-	
-def parse_complexity(merged, row):
-	name = row[1][2:]
-	complexity = row[4]
-	merged.record_detected(name, complexity)
-
-def parse_freqs(merged, row):
-	name = row[0]
-	freqs = row[1]
-	merged.extend_with(name, freqs)
-
-def merge(revs_file, comp_file):
-	merged = Merged()
-	parse_csv(merged, comp_file, parse_complexity, expected_format='language,filename,blank,comment,code')
-	parse_csv(merged, revs_file, parse_freqs, expected_format='entity,n-revs')
-	write_csv(merged.sorted_result())
-
 ######################################################################
 ## Parse input
 ######################################################################
