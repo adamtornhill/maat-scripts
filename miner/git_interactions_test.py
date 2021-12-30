72787b2c219d92e057ffb4a04a6590cefa40d6ea
Stefan Boos <kontakt@boos.systems>
2021-12-30 18:23:50 +0100

    test: document subprocess.Popen API expectation

--- miner/git_interactions_test.py
+++ miner/git_interactions_test.py
@@ -1,12 +1,17 @@
 import unittest
 import subprocess
 
+
 class GitInteractionsTest(unittest.TestCase):
-    def test_reproduce_problem(self):
-        process_stdout = subprocess.Popen([ "echo", "first line\nsecond line" ], stdout=subprocess.PIPE).communicate()[0]
-        
-        lines = process_stdout.split("\n")
-        self.assertEqual(1, len(lines))
+    def test_split_subprocess_stdout_using_utf8_encoded_linefeed(self):
+        """ Verify the assumption that subprocess.Popen returns stdout as UTF-8 encoded byte array
+        """
+        process_stdout = subprocess.Popen(["echo", "first line\nsecond line"], stdout=subprocess.PIPE).communicate()[0]
+
+        lines = process_stdout.split("\n".encode('UTF-8'))
+
+        self.assertEqual(3, len(lines))
+
 
 if __name__ == '__main__':
     unittest.main()
