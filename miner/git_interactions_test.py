db92b75ead0ccf651e1346737bc10ec30abd8bd4
Stefan Boos <kontakt@boos.systems>
2021-12-30 18:45:23 +0100

    test: reproduce exception in _read_revisions_matching

--- miner/git_interactions_test.py
+++ miner/git_interactions_test.py
@@ -1,5 +1,6 @@
 import unittest
 import subprocess
+import git_interactions
 
 
 class GitInteractionsTest(unittest.TestCase):
@@ -8,9 +9,18 @@ class GitInteractionsTest(unittest.TestCase):
         """
         process_stdout = subprocess.Popen(["echo", "first line\nsecond line"], stdout=subprocess.PIPE).communicate()[0]
 
-        lines = process_stdout.split("\n".encode('UTF-8'))
+        lines = process_stdout.split('\n'.encode('UTF-8'))
 
         self.assertEqual(3, len(lines))
+
+    def test_read_revisions_matching_should_return_three_revisions(self):
+        expected_revision = "d804759"
+        fake_git_result = expected_revision + " Documented tree map visualizations"
+        fake_git_cmd = ['echo', fake_git_result]
+
+        actual = git_interactions._read_revisions_matching(fake_git_cmd)
+
+        self.assertEqual([expected_revision], actual)
 
 
 if __name__ == '__main__':
