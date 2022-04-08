e7366e942f6bd5ffe1ef965154125e30b7ea01b2
Stefan Boos <kontakt@boos.systems>
2022-04-08 07:29:47 +0200

    test: refactor tests to test suite

--- combine-repos/combine_repos_test.py
+++ combine-repos/combine_repos_test.py
@@ -5,51 +5,51 @@ import unittest
 import combine_repos
 
 
+class TestCase:
+    def __init__(self, t, description, first_history_file, second_history_file, expected_history_file):
+        self.t = t
+        self.first_history_file = first_history_file
+        self.second_history_file = second_history_file
+        self.expected_history_file = expected_history_file
+        self.description = description
+
+    def run(self):
+        parser = combine_repos.create_argument_parser()
+
+        # GIVEN I have two git history files
+        args = [self.first_history_file, self.second_history_file]
+
+        # WHEN I combine the git history files
+        args = parser.parse_args(args)
+        buffer = io.StringIO()
+        with contextlib.redirect_stdout(buffer):
+            combine_repos.run(args)
+
+        # THEN the combined history is printed to stdout
+        # AND the combined history matches the expected file
+        with open(self.expected_history_file, 'rt') as f:
+            expected = f.read().splitlines()
+        expected.append('')
+
+        actual = buffer.getvalue().split('\n')
+
+        self.t.assertEqual(expected, actual, self.description)
+
+
 class CombineReposTest(unittest.TestCase):
-    def test_concatenate_git_history_files(self):
-        # SCENARIO Simply concatenate git history files
-        parser = combine_repos.create_argument_parser()
+    def test_suite(self):
+        suite = [
+            TestCase(self, "combined git history should be equivalent to concatenated content",
+                     "test-data/concatenate/first_evo.log", "test-data/concatenate/second_evo.log",
+                     "test-data/concatenate/combined_evo.log"),
+            TestCase(self, "combined git history should be sorted by date descending",
+                     "test-data/sort/first_evo.log", "test-data/sort/second_evo.log",
+                     "test-data/sort/combined_evo.log")
+        ]
 
-        # GIVEN I have two git history files
-        args = ["test-data/concatenate/first_evo.log", "test-data/concatenate/second_evo.log"]
-
-        # WHEN I combine the git history files
-        args = parser.parse_args(args)
-        buffer = io.StringIO()
-        with contextlib.redirect_stdout(buffer):
-            combine_repos.run(args)
-
-        # THEN the combined history is printed to stdout
-        with open('test-data/concatenate/combined_evo.log', 'rt') as f:
-            expected = f.read().splitlines()
-        expected.append('')
-
-        actual = buffer.getvalue().split('\n')
-
-        self.assertEqual(expected, actual, "combined git history should be equivalent to concatenated content")
-
-    def test_sort_combined_git_history_files(self):
-        # SCENARIO Sort combined git history files
-        parser = combine_repos.create_argument_parser()
-
-        # GIVEN I have two git history files
-        args = ["test-data/sort/first_evo.log", "test-data/sort/second_evo.log"]
-
-        # WHEN I combine the git history files
-        args = parser.parse_args(args)
-        buffer = io.StringIO()
-        with contextlib.redirect_stdout(buffer):
-            combine_repos.run(args)
-
-        # THEN the combined history is printed to stdout
-        # AND the combined history is sorted by date in descending order
-        with open('test-data/sort/combined_evo.log', 'rt') as f:
-            expected = f.read().splitlines()
-        expected.append('')
-
-        actual = buffer.getvalue().split('\n')
-
-        self.assertEqual(expected, actual, "combined git history should be sorted by date descending")
+        for case in suite:
+            with self.subTest(desc=case.description):
+                case.run()
 
 if __name__ == '__main__':
     unittest.main()
