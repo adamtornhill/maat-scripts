60ef4c97ac7e51ec3461594617e60572e6afdb87
Stefan Boos <kontakt@boos.systems>
2022-04-10 20:58:16 +0200

    feat: (wip) sort commits - introduce CommitHistoryFile

---
+++ combine-repos/commit.py
@@ -0,0 +1,41 @@
+class Commit:
+    def __init__(self):
+        self.first_line = ''
+        self.change_lines = []
+        pass
+
+    @property
+    def first_line(self):
+        return self._first_line
+
+    @first_line.setter
+    def first_line(self, value):
+        self._first_line = value
+
+    @first_line.deleter
+    def first_line(self):
+        del self._first_line
+
+    @property
+    def change_lines(self):
+        return self._change_lines
+
+    @change_lines.setter
+    def change_lines(self, value):
+        self._change_lines = value
+
+    @change_lines.deleter
+    def change_lines(self):
+        del self._change_lines
+
+    def __eq__(self, other):
+        if not isinstance(other, Commit):
+            return False
+        return self.first_line == other.first_line and self.change_lines == other.change_lines
+
+    def __repr__(self) -> str:
+        representation = 'Commit("{0}", [ '.format(self.first_line)
+        for change_line in self.change_lines:
+            representation += '"{0}" '.format(change_line)
+        representation += ']'
+        return representation
--- combine-repos/commit_history_file.py
+++ combine-repos/commit_history_file.py
@@ -1,6 +1,41 @@
+from commit import Commit
+
+
 class CommitHistoryFile:
     def __init__(self, path):
         self.path = path
+
+    def parse(self, contents):
+        if contents == '':
+            return []
+
+        commits = []
+        commit = Commit()
+
+        lines = contents.rstrip().split('\n')
+
+        index = 0
+        has_first_line = False
+        while index < len(lines):
+            line = lines[index]
+
+            if line == '':
+                commits.append(commit)
+                commit = Commit()
+                has_first_line = False
+
+            elif not has_first_line:
+                commit.first_line = line
+                has_first_line = True
+
+            else:
+                commit.change_lines.append(line)
+
+            index += 1
+
+        commits.append(commit)
+
+        return commits
 
     def read(self):
         with open(self.path, 'rt') as f:
