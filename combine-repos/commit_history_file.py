2f14782caf3284c50ad192719021d54c96533f60
Stefan Boos <kontakt@boos.systems>
2022-04-12 08:42:06 +0200

    refactor: simplify parsing the CommitHistoryFile

--- combine-repos/commit_history_file.py
+++ combine-repos/commit_history_file.py
@@ -1,6 +1,30 @@
 from commit import Commit
 
 
+def group_lines_by_commit(lines):
+    """Group lines into "commit_groups" which are not separated by a blank line.
+    Every commit is a group of lines.
+    Two commits are separated by a blank line.
+
+    Returns a list of string lists. Each string list represents a commit.
+    """
+
+    commit_groups = []
+    current_commit_group = []
+    for line in lines:
+        if line != '':
+            current_commit_group.append(line)
+        else:
+            commit_groups.append(current_commit_group)
+            current_commit_group = []
+
+    # If the file does not end with an empty line, the last commit group must be added here.
+    if len(current_commit_group) > 0:
+        commit_groups.append(current_commit_group)
+
+    return commit_groups
+
+
 class CommitHistoryFile:
     def __init__(self, path):
         self.path = path
@@ -9,31 +33,17 @@ class CommitHistoryFile:
         if contents == '':
             return []
 
+        # split the file contents by line
+        lines = contents.split('\n')
+
+        commit_groups = group_lines_by_commit(lines)
+
         commits = []
-        commit = Commit()
-
-        lines = contents.rstrip().split('\n')
-
-        index = 0
-        has_first_line = False
-        while index < len(lines):
-            line = lines[index]
-
-            if line == '':
-                commits.append(commit)
-                commit = Commit()
-                has_first_line = False
-
-            elif not has_first_line:
-                commit.first_line = line
-                has_first_line = True
-
-            else:
-                commit.change_lines.append(line)
-
-            index += 1
-
-        commits.append(commit)
+        for commit_group in commit_groups:
+            commit = Commit()
+            commit.first_line = commit_group[0]
+            commit.change_lines = commit_group[1:]
+            commits.append(commit)
 
         return commits
 
