6d2c412c758d70916f11b274db66e0c85b6c310a
Stefan Boos <kontakt@boos.systems>
2022-04-25 06:02:39 +0200

    test: (wip) create first test for CommitListPrinter

--- combine-repos/commit_list_printer.py
+++ combine-repos/commit_list_printer.py
@@ -1,10 +1,14 @@
 class CommitListPrinter:
+
+    def __init__(self) -> None:
+        self.print_function = print
+
     # TODO test boundary cases
     # - empty commits array
     # - single commit
     # - two commits
     def print(self, commits):
         for commit in commits[:-1]:
-            print(str(commit))
-            print()
-        print(str(commits[-1]))
+            self.print_function(str(commit))
+            self.print_function()
+        self.print_function(str(commits[-1]))
---
+++ combine-repos/test_commit_list_printer.py
@@ -0,0 +1,30 @@
+import unittest
+
+from commit import Commit
+from commit_list_printer import CommitListPrinter
+
+
+class CommitListPrinterTest(unittest.TestCase):
+    def __init__(self, method_name: str = ...) -> None:
+        super().__init__(method_name)
+        self.printed = []
+
+    def test(self):
+        commits = [self.create_commit(), self.create_commit()]
+
+        sut = CommitListPrinter()
+        sut.print_function = self.print_replacement
+        sut.print(commits)
+
+        # TODO fix assert statement
+        self.assertEqual(0, 0)
+
+    def print_replacement(self, string=''):
+        self.printed.append(string)
+
+    @staticmethod
+    def create_commit():
+        result = Commit()
+        result.first_line = "[1234567] Some Author 2022-04-25 Some Commit Message"
+        result.change_lines = ["1\t2\tsome_file.txt", "3\t4\tanother_file.txt"]
+        return result
