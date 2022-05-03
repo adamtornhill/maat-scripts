b426ac91fd01a1e736c835c90994f333d23c384c
Stefan Boos <kontakt@boos.systems>
2022-05-03 08:04:01 +0200

    refactor(tests): allow arbitrary number of TestCase input files

--- combine-repos/test_combine_repos.py
+++ combine-repos/test_combine_repos.py
@@ -6,18 +6,17 @@ from capture_stdout_to_list import CaptureStdoutToList
 
 
 class TestCase:
-    def __init__(self, t, description, first_history_file, second_history_file, expected_history_file):
+    def __init__(self, t, description, input_history_files, expected_history_file):
         self.t = t
-        self.first_history_file = first_history_file
-        self.second_history_file = second_history_file
+        self.input_history_files = input_history_files
         self.expected_history_file = expected_history_file
         self.description = description
 
     def run(self):
         parser = combine_repos.create_argument_parser()
 
-        # GIVEN I have two git history files
-        args = [self.first_history_file, self.second_history_file]
+        # GIVEN I have a number of git history files
+        args = self.input_history_files
 
         # WHEN I combine the git history files
         args = parser.parse_args(args)
@@ -37,11 +36,11 @@ class CombineReposTest(unittest.TestCase):
     def test_suite(self):
         suite = [
             TestCase(self, "1 commit per file => simply concatenate the files",
-                     "test-data/concatenate/first_evo.log", "test-data/concatenate/second_evo.log",
+                     ["test-data/concatenate/first_evo.log", "test-data/concatenate/second_evo.log"],
                      "test-data/concatenate/combined_evo.log"),
 
             TestCase(self, "multiple commits per file => result is sorted by date descending",
-                     "test-data/sort/first_evo.log", "test-data/sort/second_evo.log",
+                     ["test-data/sort/first_evo.log", "test-data/sort/second_evo.log"],
                      "test-data/sort/combined_evo.log")
         ]
 
