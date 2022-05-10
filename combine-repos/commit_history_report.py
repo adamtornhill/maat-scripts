99181b9e4ae6d426c576a7d89691a4e4917163ff
Stefan Boos <kontakt@boos.systems>
2022-05-10 07:55:01 +0200

    feat: no blank between merge and ordinary commit in CommitHistoryReport

--- combine-repos/commit_history_report.py
+++ combine-repos/commit_history_report.py
@@ -8,7 +8,12 @@ class CommitHistoryReport:
         lines = []
         for commit in commits[:-1]:
             lines.append(str(commit))
-            lines.append('')
+
+            # Only insert the blank line, if the current commit is not a merge commit
+            # merge commits can be recognized by not having the numstat entry - i.e. the change records in
+            # the change_lines property
+            if len(commit.change_lines) > 0:
+                lines.append('')
 
         lines.append(str(commits[-1]))
         return '\n'.join(lines)
